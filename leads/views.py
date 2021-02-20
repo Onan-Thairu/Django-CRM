382d96139ec1c8a6b2bfba4a4bc5925e1634e3f4
Onan Thairu <thairuonan@gmail.com>
2021-02-20 11:15:35 +0300

    Added templates and a form

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Lead
+from .forms import LeadForm
 
-# Create your views here.
+def lead_list(request):
+    leads = Lead.objects.all()
+
+    context = {
+        'leads': leads
+    }
+    return render(request, "leads/lead_list.html", context)
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        'lead':lead
+    }
+    return render(request, 'leads/lead_detail.html', context)
+
+def lead_create(request):
+    form = LeadForm()
+    if request.method == 'POST':
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            print("The form has been validated")
+            print(form.cleaned_data )
+    context = {
+        'form': form
+    }
+    return render(request, 'leads/lead_create.html', context)
