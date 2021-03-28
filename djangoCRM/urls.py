42af6093fe9807dcc8938e782ecefea2c9d76837
Onan Thairu <thairuonan@gmail.com>
2021-03-28 10:15:28 +0300

    set up static files

--- djangoCRM/urls.py
+++ djangoCRM/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import landing_page, LandingPageView
@@ -7,4 +9,9 @@ urlpatterns = [
     path('', LandingPageView.as_view(), name="landing-page"),
     #path('', landing_page, name="landing-page"),
     path('leads/', include('leads.urls', namespace="leads")),
+
+    
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
