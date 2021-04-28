9a22fa09c6542af063fb5820af1e8656db559bf8
Onan Thairu <thairuonan@gmail.com>
2021-04-28 21:25:14 +0300

    Added mixins, signals and agents app

--- leads/models.py
+++ leads/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
@@ -15,6 +23,13 @@ class Lead(models.Model):
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organization = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.email
+
+def post_user_created_signal(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(post_user_created_signal, sender=User)
--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
 #from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 #TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Lead
@@ -22,7 +23,7 @@ def landing_page(request):
     return render(request, "landing.html")
 
 
-class LeadListView(generic.ListView):
+class LeadListView(LoginRequiredMixin, generic.ListView):
     template_name = "leads/lead_list.html"
     queryset = Lead.objects.all()
     context_object_name = "leads"
@@ -37,7 +38,7 @@ def lead_list(request):
     return render(request, "leads/lead_list.html", context)
 
 
-class LeadDetailView(generic.DetailView):
+class LeadDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = "leads/lead_detail.html"
     queryset = Lead.objects.all()
     context_object_name = "lead"
@@ -50,7 +51,7 @@ def lead_detail(request, pk):
     return render(request, 'leads/lead_detail.html', context)
 
 
-class LeadCreateView(generic.CreateView):
+class LeadCreateView(LoginRequiredMixin, generic.CreateView):
     template_name = "leads/lead_create.html"
     form_class = LeadModelForm
 
@@ -79,7 +80,7 @@ def lead_create(request):
     return render(request, 'leads/lead_create.html', context)
 
 
-class LeadUpdateView(generic.UpdateView):
+class LeadUpdateView(LoginRequiredMixin, generic.UpdateView):
     template_name = "leads/lead_update.html"
     queryset = Lead.objects.all()
     form_class = LeadModelForm
@@ -104,7 +105,7 @@ def lead_update(request, pk):
 
 
 
-class LeadDeleteView(generic.DeleteView):
+class LeadDeleteView(LoginRequiredMixin, generic.DeleteView):
     template_name = "leads/lead_delete.html"
     queryset = Lead.objects.all()
 
