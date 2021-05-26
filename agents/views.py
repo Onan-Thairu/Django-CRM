e4403b05853cd7d8254d44d04dd4e56158c7d069
Onan Thairu <thairuonan@gmail.com>
2021-05-26 18:48:13 +0300

    Implemented password reset

--- agents/views.py
+++ agents/views.py
@@ -1,9 +1,11 @@
+import random
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import reverse
 from leads.models import Agent
 from .forms import AgentModelForm
 from .mixins import OrganizorAndLoginRequiredMixin
+from django.core.mail import send_mail
 
 class AgentListView(OrganizorAndLoginRequiredMixin, generic.ListView):
     template_name = "agents/agent_list.html"
@@ -20,9 +22,22 @@ class AgentCreateView(OrganizorAndLoginRequiredMixin, generic.CreateView):
         return reverse("agents:agent-list")
 
     def form_valid(self, form):
-        agent = form.save(commit=False)
-        agent.organization = self.request.user.userprofile
-        agent.save()
+        user = form.save(commit=False)
+        user.is_organizor = False
+        user.is_agent = True
+        user.set_password(f"{random.randint(0, 1000000)}")
+        user.save()
+
+        Agent.objects.create(
+            user=user,
+            organization = self.request.user.userprofile
+        )
+        send_mail(
+            subject = "You are invited to be an agent",
+            message = "You were added as agent on DJ CRM. Please login to start working.",
+            from_email = "test@gmail.com",
+            recipient_list = [user.email]
+        )
         return super(AgentCreateView, self).form_valid(form)
 
 class AgentDetailView(OrganizorAndLoginRequiredMixin, generic.DetailView):
